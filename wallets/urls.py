5bea3116c5bb41be8a87aa8a85be6886353843d8
anyacodelab <anyacodelab@gmail.com>
2025-06-26 23:23:11 +0100

    Add wallet kyc fix

--- wallets/urls.py
+++ wallets/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WalletViewSet, WalletMemberViewSet, WalletInvitationViewSet, KYCVerificationView
+from .views import WalletViewSet, WalletMemberViewSet, WalletInvitationViewSet, KYCVerificationViewSet
 
 
 router = DefaultRouter()
 router.register('wallets', WalletViewSet)
 router.register('wallet-members', WalletMemberViewSet)
 router.register('wallet-invitation', WalletInvitationViewSet)
+router.register(r'kyc', KYCVerificationViewSet, basename='kyc')
 
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('kyc/verify/', KYCVerificationView.as_view(), name='kyc_verification_page'),
+    path('kyc/verify/', KYCVerificationViewSet.as_view(), name='kyc_verification_page'),
 ]
 
 
--- wallets/views.py
+++ wallets/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.decorators import action, IsAuthenticated
 from django.utils import timezone
 from .models import Wallet, WalletMember, WalletInvitation
 from .serializers import WalletSerializer, WalletMemberSerializer, WalletInvitationSerializer, KYCVerificationSerializer
